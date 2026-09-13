f1890640ce87f667b2e6b847505c774eef371cfc
Yadav Subba <yadavsubba2003@gmail.com>
2026-09-13 20:01:41 +0530

    Change Firebase API key in script.js
    Updated Firebase API key in configuration.

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ import { getDatabase, ref, get, push, serverTimestamp }
     from "https://www.gstatic.com/firebasejs/12.17.1/firebase-database.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAoPVLSklKARDfdDo6m2zkj1kabJVpsk",
+    apiKey: "AIzaSyAoPVLSklKARDfdDoSm6L2zkj1kabJVpsw",
     authDomain: "rudrabhakti-a1d3e.firebaseapp.com",
     databaseURL: "https://rudrabhakti-a1d3e-default-rtdb.firebaseio.com",
     projectId: "rudrabhakti-a1d3e",
